0187504ed3157c3e01da2a2c4ca0ddd264c64131
Yaroslav Petrukhin <yar.petrukhin@gmail.com>
2023-06-19 23:17:01 +0300

    socket.io sessions counter added

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-
 const productsRouter = require('./routes/api/products');
 const ordersRouter = require('./routes/api/orders');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = require('http').Server(app);
+const io = socketIO(server);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-
 
 app.use('/api/products', productsRouter);
 app.use('/api/orders', ordersRouter);
@@ -26,5 +27,7 @@ app.use((error, __, response, _) => {
   const { status = 500, message = 'Server error' } = error;
   response.status(status).json({ message });
 });
+
+app.set('socketio', io);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const socketIO = require('socket.io');
 const app = require('./app');
 
 mongoose.set('strictQuery', true);
@@ -9,7 +10,21 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log('DB connect success');
-    app.listen(PORT, () => console.log(`Server is running... . Use API on port ${PORT}`));
+
+    const server = app.listen(PORT, () =>
+      console.log(`Server is running... . Use API on port ${PORT}`)
+    );
+    const io = socketIO(server);
+    let activeSessions = 0;
+
+    io.on('connection', socket => {
+      activeSessions += 1;
+      io.emit('activeSessionCount', activeSessions);
+      socket.on('disconnect', () => {
+        activeSessions--;
+        io.emit('activeSessionCount', activeSessions);
+      });
+    });
   })
   .catch(error => {
     console.log(error.message);
